28f9c2f55f76df19b6d22206b5f66df0e3df3148
urazlo <assaurasins@gmail.com>
2020-06-11 22:20:16 +0300

    Refactored event.key listener

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ const frontTetris = document.getElementsByClassName('game-page__tetris-front')[0
 const excel = document.getElementsByClassName('excel');
 const cells = document.getElementsByClassName('excel');
 const inicialX = 5;
-const inicialY = 10;
+const inicialY = 15;
+
+const interval = setInterval(() => {
+    move();
+}, 1000);
+
 const figuresArray = [
     // вертикальная палка
     [
@@ -62,14 +67,18 @@ const figuresArray = [
 
 let currentFigure = 0;
 let figureBody = 0;
-//Обработчики событий
+let rowCounter = 0;
+let flag = true;
 
-// Отображение уровня
+//ОБРАБОТЧИКИ СОБЫТИЙ
+
+//Отображение уровня
 level.addEventListener('mouseup', () => {
     const value = level.value;
     document.getElementById('menu-page-lvl').textContent = `Level: ${value}`;
     document.getElementById('game-page-lvl').textContent = `Level: ${value}`;
 });
+
 // Переключатель меню 
 menuHider.addEventListener('click', () => {
     document.querySelector('.menu-page').classList.toggle('hidden');
@@ -80,7 +89,57 @@ openMenu.addEventListener('click', () => {
     document.querySelector('.game-page').classList.toggle('hidden');
 });
 
-//Добавление клеток
+// Отключение действий браузера по умолчанию
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode == 40 || event.keyCode == 38) {
+        event.preventDefault();
+    }
+});
+
+// Перемещение фигуры по нажатию клавиш
+window.addEventListener('keydown', function(event) {
+
+    let coordinates1 = [figureBody[0].getAttribute('posX'), figureBody[0].getAttribute('posY')];
+    let coordinates2 = [figureBody[1].getAttribute('posX'), figureBody[1].getAttribute('posY')];
+    let coordinates3 = [figureBody[2].getAttribute('posX'), figureBody[2].getAttribute('posY')];
+    let coordinates4 = [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')];
+
+    function getNewState(direction) {
+        flag = true;
+        let figureNew = [
+            document.querySelector(`[posX = "${+coordinates1[0]+direction}"][posY= "${coordinates1[1]}"]`),
+            document.querySelector(`[posX = "${+coordinates2[0]+direction}"][posY= "${coordinates2[1]}"]`),
+            document.querySelector(`[posX = "${+coordinates3[0]+direction}"][posY= "${coordinates3[1]}"]`),
+            document.querySelector(`[posX = "${+coordinates4[0]+direction}"][posY= "${coordinates4[1]}"]`),
+        ];
+
+        for (let i = 0; i < figureNew.length; i++) {
+            if (!figureNew[i] || figureNew[i].classList.contains('set')) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            for (let i = 0; i < figureBody.length; i++) {
+                figureBody[i].classList.remove('figure');
+            }
+
+            figureBody = figureNew;
+
+            for (let i = 0; i < figureBody.length; i++) {
+                figureBody[i].classList.add('figure');
+            }
+        }
+    }
+    if (event.keyCode == 37) {
+        getNewState(-1);
+    } else if (event.keyCode == 39) {
+        getNewState(1);
+    } else if (event.keyCode == 40) {
+        move();
+    }
+});
+
+//Заполнение игрового поля
 
 function drawFill() {
     backTetris.classList.add('game-page__tetris-back');
@@ -96,20 +155,17 @@ function drawFill() {
 //Присвоение клеткам координат x/y
 
 function setCellsCoorinates() {
-
-    let i = 0;
-
     for (let y = 18; y > 0; y--) {
         for (let x = 1; x <= 10; x++) {
-            excel[i].setAttribute('posX', x);
-            excel[i].setAttribute('posY', y);
-            i++;
+            excel[rowCounter].setAttribute('posX', x);
+            excel[rowCounter].setAttribute('posY', y);
+            rowCounter++;
         }
     }
 }
 
 //Получение и отрисовка случайной фигуры
-function getRandomFigure() {
+function getFigure() {
     function getRandom() {
         return Math.round(Math.random() * (figuresArray.length - 1));
     }
@@ -126,6 +182,46 @@ function getRandomFigure() {
         figureBody[i].classList.add('figure');
     }
 }
+
+// Передвижение фигуры с проверкой столкновения
+
+function move() {
+    let moveFlag = true;
+    let coordinates = [
+        [figureBody[0].getAttribute('posX'), figureBody[0].getAttribute('posY')],
+        [figureBody[1].getAttribute('posX'), figureBody[1].getAttribute('posY')],
+        [figureBody[2].getAttribute('posX'), figureBody[2].getAttribute('posY')],
+        [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')],
+    ];
+
+    for (let i = 0; i < coordinates.length; i++) {
+        if (coordinates[i][1] == 1 || document.querySelector(`[posX = "${coordinates[i][0]}"][posY= "${coordinates[i][1]-1}"]`).classList.contains('set')) {
+            moveFlag = false;
+            break;
+        }
+    }
+    if (moveFlag) {
+        for (let i = 0; i < figureBody.length; i++) {
+            figureBody[i].classList.remove('figure');
+        }
+        figureBody = [
+            document.querySelector(`[posX = "${coordinates[0][0]}"][posY= "${coordinates[0][1]-1}"]`),
+            document.querySelector(`[posX = "${coordinates[1][0]}"][posY= "${coordinates[1][1]-1}"]`),
+            document.querySelector(`[posX = "${coordinates[2][0]}"][posY= "${coordinates[2][1]-1}"]`),
+            document.querySelector(`[posX = "${coordinates[3][0]}"][posY= "${coordinates[3][1]-1}"]`),
+        ];
+        for (let i = 0; i < figureBody.length; i++) {
+            figureBody[i].classList.add('figure');
+        }
+    } else {
+        for (let i = 0; i < figureBody.length; i++) {
+            figureBody[i].classList.remove('figure');
+            figureBody[i].classList.add('set');
+        }
+        getFigure();
+    }
+}
+
 
 //Устанавливаем размеры клетки
 function setCellSize() {
@@ -148,5 +244,5 @@ function setTetrisFieldSize() {
 drawFill();
 setCellsCoorinates();
 setCellSize();
-getRandomFigure();
+getFigure();
 setTetrisFieldSize();
